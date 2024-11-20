803b1ab38a08f05d287bac35c768fe4c86fa4923
yusufadi <dragonangun@gmail.com>
2024-11-20 11:42:04 +0700

    YusupAdi13

---
+++ src/kelas/Sesi.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelas;
+
+/**
+ *
+ * @author MyBook Hype
+ */
+public class Sesi {
+    private static String user,nama,email,status;
+
+    public static String getUser() {
+        return user;
+    }
+
+    public static void setUser(String user) {
+        Sesi.user = user;
+    }
+
+    public static String getNama() {
+        return nama;
+    }
+
+    public static void setNama(String nama) {
+        Sesi.nama = nama;
+    }
+
+    public static String getEmail() {
+        return email;
+    }
+
+    public static void setEmail(String email) {
+        Sesi.email = email;
+    }
+
+    public static String getStatus() {
+        return status;
+    }
+
+    public static void setStatus(String status) {
+        Sesi.status = status;
+    }
+    
+}
